aac0f74331d071c7003d301733bcc11c53a33aac
whharris917 <whharris@ncsu.edu>
2017-06-05 11:17:39 -0600

    Update SetupReconMultiRes.m

--- src/matlab/SetupReconMultiRes.m
+++ src/matlab/SetupReconMultiRes.m
@@ -1,32 +1,34 @@
 function ReconH = SetupReconMultiRes(params)
 
-ReconH = params;
-ReconH.ReconObjects = cell(params.NUM_LEVELS, 1);
-ReconH.NUM_EXEMPLARS = length(ReconH.EXEMPLARS);
-P = ReconH.EXEMPLARS;
-NUM_LEVELS = length(P{1});
-ReconH.ReconObjects = cell(NUM_LEVELS, 1);
-RECON_SIZE = params.FULL_RECON_SIZE;
+    ReconH = params;
+    ReconH.ReconObjects = cell(params.NUM_LEVELS, 1);
+    ReconH.NUM_EXEMPLARS = length(ReconH.EXEMPLARS);
+    P = ReconH.EXEMPLARS;
+    NUM_LEVELS = length(P{1});
+    ReconH.ReconObjects = cell(NUM_LEVELS, 1);
+    RECON_SIZE = params.FULL_RECON_SIZE;
 
-for level=1:ReconH.NUM_LEVELS
+    for level=1:ReconH.NUM_LEVELS
 
-    fprintf(1, 'Level %d\n', level);
-    
-    Exyz = cell(ReconH.NUM_EXEMPLARS,1);
-    for ex=1:ReconH.NUM_EXEMPLARS
-        Exyz{ex} = P{ex}{level};
+        fprintf(1, 'Level %d\n', level);
+
+        Exyz = cell(ReconH.NUM_EXEMPLARS,1);
+        for ex=1:ReconH.NUM_EXEMPLARS
+            Exyz{ex} = P{ex}{level};
+        end
+
+        rparams.NUM_CORES = params.NUM_CORES;
+        rparams.NB_SIZE = params.NB_SIZES(level);
+        rparams.NB_INDICES = params.NB_INDICES;
+        rparams.RECON_SIZE = RECON_SIZE;
+        rparams.EXEMPLARS = Exyz;
+        rparams.ANN_ALGO = params.ANN_ALGO;
+        rparams.UseNeighborhoodSearchWeights = params.UseNeighborhoodSearchWeights;
+
+        ReconH.ReconObjects{level} = SetupRecon(rparams);
+
+        RECON_SIZE = ceil(RECON_SIZE / 2);
+
     end
-    
-    rparams.NUM_CORES = params.NUM_CORES;
-    rparams.NB_SIZE = params.NB_SIZES(level);
-    rparams.NB_INDICES = params.NB_INDICES;
-    rparams.RECON_SIZE = RECON_SIZE;
-    rparams.EXEMPLARS = Exyz;
-    rparams.ANN_ALGO = params.ANN_ALGO;
-    rparams.UseNeighborhoodSearchWeights = params.UseNeighborhoodSearchWeights;
-    
-    ReconH.ReconObjects{level} = SetupRecon(rparams);
-    
-    RECON_SIZE = ceil(RECON_SIZE / 2);
-    
+
 end
