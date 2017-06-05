5388f7238d12f164eeda0a0d5331839d67b5bfb3
whharris917 <whharris@ncsu.edu>
2017-06-05 07:54:35 -0600

    Update SetupReconMultiRes.m

--- src/matlab/SetupReconMultiRes.m
+++ src/matlab/SetupReconMultiRes.m
@@ -3,21 +3,13 @@ function ReconH = SetupReconMultiRes(params)
 ReconH = params;
 ReconH.ReconObjects = cell(params.NUM_LEVELS, 1);
 ReconH.NUM_EXEMPLARS = length(ReconH.EXEMPLARS);
-
-if(iscell(ReconH.EXEMPLARS{1}))
-    P = ReconH.EXEMPLARS;
-    NUM_LEVELS = length(P{1});
-    ReconH.ReconObjects = cell(NUM_LEVELS, 1);
-else
-    P = cell(ReconH.NUM_EXEMPLARS, 1);
-    for ii=1:ReconH.NUM_EXEMPLARS
-        P{ii} = BuildExemplarPyramid(ReconH.EXEMPLARS{ii});
-    end
-end
-
+P = ReconH.EXEMPLARS;
+NUM_LEVELS = length(P{1});
+ReconH.ReconObjects = cell(NUM_LEVELS, 1);
 RECON_SIZE = params.FULL_RECON_SIZE;
 
 for level=1:ReconH.NUM_LEVELS
+
     fprintf(1, 'Level %d\n', level);
     
     Exyz = cell(ReconH.NUM_EXEMPLARS,1);
@@ -25,25 +17,16 @@ for level=1:ReconH.NUM_LEVELS
         Exyz{ex} = P{ex}{level};
     end
     
-    if(~isfield(params, 'NUM_CORES'))
-        rparams.NUM_CORES = 1;
-    else
-        rparams.NUM_CORES = params.NUM_CORES;
-    end
-    
+    rparams.NUM_CORES = params.NUM_CORES;
     rparams.NB_SIZE = params.NB_SIZES(level);
     rparams.NB_INDICES = params.NB_INDICES;
     rparams.RECON_SIZE = RECON_SIZE;
     rparams.EXEMPLARS = Exyz;
-    
-    if(isfield(params, 'ANN_ALGO'))
-        rparams.ANN_ALGO = params.ANN_ALGO;
-    end
-    
-    if(isfield(params, 'UseNeighborhoodSearchWeights'))
-        rparams.UseNeighborhoodSearchWeights = params.UseNeighborhoodSearchWeights;
-    end
+    rparams.ANN_ALGO = params.ANN_ALGO;
+    rparams.UseNeighborhoodSearchWeights = params.UseNeighborhoodSearchWeights;
     
     ReconH.ReconObjects{level} = SetupRecon(rparams);
+    
     RECON_SIZE = ceil(RECON_SIZE / 2);
+    
 end
