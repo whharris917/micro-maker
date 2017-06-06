48eb1dc41fa0f082e423cf97ade019364e0997dc
whharris917 <whharris@ncsu.edu>
2017-06-06 11:55:21 -0600

    Update SolidOptimization.m

--- src/matlab/SolidOptimization.m
+++ src/matlab/SolidOptimization.m
@@ -17,27 +17,8 @@ function [S, Recon] = SolidOptimization(Recon, NUM_ITERATIONS, start, useWeights
         tic;
         fprintf(1, '    Optimizing solid ... ');
         [Snew, Recon.TexelWeights, Recon.SourceTable] = OptimizeSolidMEX(S, Recon);
-
-        Recon.NBHoodHist = cell(length(Recon.EXEMPLARS),1);
-        for pp=1:size(Recon.EXEMPLARS, 1)
-            H = zeros(size(Recon.EXEMPLARS{pp})); 
-            NBs = Recon.NNB_Table(:, :, :, pp); 
-            NBs = NBs(:);
-            NBs = NBs(NBs > 0);
-            for kk=1:length(NBs) 
-                xy = Recon.NB_ExemplarLookup{pp}(NBs(kk), :); 
-                H(xy(1), xy(2)) = H(xy(1), xy(2)) + 1; 
-            end
-            H = H ./ sum(H(:));
-            Recon.NBHoodHist{pp} = H;
-        end
         
         PlotIteration(Recon, S, Snew, useWeights);
-
-        perVoxelPercentChange = abs(S(:) - Snew(:)) ./ S(:); 
-        perVoxelPercentChange(isnan(perVoxelPercentChange)) = 0;
-        perVoxelPercentChange(isinf(perVoxelPercentChange)) = 1;
-        percentChange = 100*(sum(perVoxelPercentChange) / numel(S));
           
         % It converged!
         if( ii == NUM_ITERATIONS )
@@ -47,7 +28,7 @@ function [S, Recon] = SolidOptimization(Recon, NUM_ITERATIONS, start, useWeights
             S = Snew;
         end
 
-        fprintf(1, 'Done: %f, vf=%f seconds, percentChange=%f\n', toc, mean(S(:)), percentChange);
+        fprintf(1, 'Done: %f, vf=%f seconds', toc);
         elapsed_opt = elapsed_opt + toc;
 
     end
